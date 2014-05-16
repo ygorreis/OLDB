a98fd03bcba9d1d56d62f281bc13d54331bd47f1
JonasS <JonasS@192.168.0.104>
2014-05-16 02:12:18 -0300

    Imagens Diagramas Máximo Adicionadas

--- application/views/notacoes_er.php
+++ application/views/notacoes_er.php
@@ -246,8 +246,8 @@
         </div>
         <div class="col-sm-9 col-md-9">
             <div class="well" id="apresentacao">
-                <span id="texto"><h1>Notações Entidade Relacionamento</h1>
-                Explore os varios conjuntos de notações para diagrama de entidades e relacionamentos</span>
+                <div id="texto"><h1>Notações Entidade Relacionamento</h1>
+                Explore os varios conjuntos de notações para diagrama de entidades e relacionamentos</div>
                 <img id="imagem" src="">
             </div>
         </div>
@@ -261,66 +261,10 @@
         </div>
         <!-- /container -->
  
- 
+ x
         <script type="text/javascript" src="../js/jquery-1.11.1.min.js"></script>
         <script type="text/javascript" src="../js/bootstrap.min.js"></script>
-        <script type="text/javascript">
-           $(function() {
-              $('.max_1').click(function(){
-              $("#texto").hide();
-              $("#imagem").attr('src',"<?php echo base_url().'../img/max_1.jpg' ?>");
-              return false;
-            });
-           });
-           
-           $(function() {
-              $('.max_2').click(function(){
-              $("#texto").hide();
-              $("#imagem").attr('src',"<?php echo base_url().'../img/max_2.jpg' ?>");
-              return false;
-            });
-           });
-           
-           $(function() {
-              $('.max_3').click(function(){
-              $("#texto").hide();
-              $("#imagem").attr('src',"<?php echo base_url().'../img/max_3.jpg' ?>");
-              return false;
-            });
-           });
-           
-           $(function() {
-              $('.max_4').click(function(){
-              $("#texto").hide();
-              $("#imagem").attr('src',"<?php echo base_url().'../img/max_4.jpg' ?>");
-              return false;
-            });
-           });
-           
-           $(function() {
-              $('.max_5').click(function(){
-              $("#texto").hide();
-              $("#imagem").attr('src',"<?php echo base_url().'../img/max_5.jpg' ?>");
-              return false;
-            });
-           });
-           
-           $(function() {
-              $('.max_6').click(function(){
-              $("#texto").hide();
-              $("#imagem").attr('src',"<?php echo base_url().'../img/max_6.jpg' ?>");
-              return false;
-            });
-           });
-           
-           $(function() {
-              $('.max_7').click(function(){
-              $("#texto").hide();
-              $("#imagem").attr('src',"<?php echo base_url().'../img/max_7.jpg' ?>");
-              return false;
-            });
-           });
-        </script>
+        <script type="text/javascript" src="../js/javascript_menu.js"></script>
         <!-- Bootstrap core JavaScript
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
---
+++ js/javascript_menu.js
@@ -0,0 +1,55 @@
+$(function() {
+   $('.max_1').click(function(){
+   $("#texto").hide();
+   $("#imagem").attr('src',"<?php echo base_url().'../img/max_1.jpg' ?>");
+   return false;
+ });
+});
+
+$(function() {
+   $('.max_2').click(function(){
+   $("#texto").hide();
+   $("#imagem").attr('src',"<?php echo base_url().'../img/max_2.jpg' ?>");
+   return false;
+ });
+});
+
+$(function() {
+   $('.max_3').click(function(){
+   $("#texto").hide();
+   $("#imagem").attr('src',"<?php echo base_url().'../img/max_3.jpg' ?>");
+   return false;
+ });
+});
+
+$(function() {
+   $('.max_4').click(function(){
+   $("#texto").hide();
+   $("#imagem").attr('src',"<?php echo base_url().'../img/max_4.jpg' ?>");
+   return false;
+ });
+});
+
+$(function() {
+   $('.max_5').click(function(){
+   $("#texto").hide();
+   $("#imagem").attr('src',"<?php echo base_url().'../img/max_5.jpg' ?>");
+   return false;
+ });
+});
+
+$(function() {
+   $('.max_6').click(function(){
+   $("#texto").hide();
+   $("#imagem").attr('src',"<?php echo base_url().'../img/max_6.jpg' ?>");
+   return false;
+ });
+});
+
+$(function() {
+   $('.max_7').click(function(){
+   $("#texto").hide();
+   $("#imagem").attr('src',"<?php echo base_url().'../img/max_7.jpg' ?>");
+   return false;
+ });
+});
